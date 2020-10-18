a4cc0484653b41f2c6047b2dce1b2659fa8f1b8c
rulsaniv <ruslan@rsln.dev>
2020-10-18 21:17:49 +0700

    Add Brand class to Shop app model

--- shop/admin.py
+++ shop/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Brand
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-  list_display = ['name', 'slug']
-  prepopulated_fields = {'slug': ('name',)}
+  list_display = ['category_name', 'slug']
+  prepopulated_fields = {'slug': ('category_name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+  list_display = ['brand_name', 'slug']
+  prepopulated_fields = {'slug': ('brand_name',)}
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+  list_display = ['product_name', 'brand', 'slug', 'price', 'available', 'created', 'updated']
   list_filter = ['available', 'created', 'updated']
   list_editable = ['price', 'available']
-  prepopulated_fields = {'slug': ('name',)}
+  prepopulated_fields = {'slug': ('product_name',)}
--- shop/models.py
+++ shop/models.py
@@ -2,23 +2,37 @@ from django.db import models
 
 
 class Category(models.Model):
-  name = models.CharField(max_length=128, db_index=True)
+  category_name = models.CharField(max_length=128, db_index=True)
   slug = models.SlugField(max_length=128, unique=True)
 
   class Meta:
-    ordering = ('name',)
+    ordering = ('category_name',)
     verbose_name = 'category'
     verbose_name_plural = 'categories'
 
   def __str__(self):
-    return self.name
+    return self.category_name
+
+
+class Brand(models.Model):
+  brand_name = models.CharField(max_length=128, db_index=True)
+  slug = models.SlugField(max_length=128, unique=True)
+
+  class Meta:
+    ordering = ('brand_name',)
+    verbose_name = 'brand'
+    verbose_name_plural = 'brands'
+
+  def __str__(self):
+    return self.brand_name
 
 
 class Product(models.Model):
-  category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-  name = models.CharField(max_length=256, db_index=True)
+  category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
+  brand = models.ForeignKey(Brand, related_name='products', on_delete=models.CASCADE)
+  product_name = models.CharField(max_length=256, db_index=True)
   slug = models.SlugField(max_length=256, db_index=True)
-  image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+  image = models.ImageField(upload_to='assets/productimages/%Y/%m/%d', blank=True)
   description = models.TextField(blank=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
   available = models.BooleanField(default=True)
@@ -26,8 +40,8 @@ class Product(models.Model):
   updated = models.DateTimeField(auto_now=True)
 
   class Meta:
-    ordering = ('name',)
+    ordering = ('product_name',)
     index_together = (('id', 'slug'),)
 
   def __str__(self):
-    return self.name
+    return self.product_name
