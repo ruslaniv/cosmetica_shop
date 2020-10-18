95d699bad5090bc1a2e868b42abe1bde4f61f06b
rulsaniv <ruslan@rsln.dev>
2020-10-18 17:31:20 +0700

    Add env file with environment variables
    All environment information from settings.py has been moved to .env
    file

--- cosmetica_shop/settings.py
+++ cosmetica_shop/settings.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ay459b7g(u*(q7ijt5r57a_sc5^16lw)nv)zj=s$57@5vo@z5p'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -81,8 +89,7 @@ WSGI_APPLICATION = 'cosmetica_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'default': env.db(),
     }
 }
 
