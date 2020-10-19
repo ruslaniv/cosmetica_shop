3b22ac941bea971c20cb3a8e102b2204cceaa76f
rulsaniv <ruslan@rsln.dev>
2020-10-19 14:30:43 +0700

    Add product usage field in product model
    Add product usage display in product detail template

--- shop/models.py
+++ shop/models.py
@@ -41,6 +41,7 @@ class Product(models.Model):
   slug = models.SlugField(max_length=256, db_index=True)
   image = models.ImageField(upload_to='assets/productimages/%Y/%m/%d', blank=True)
   description = models.TextField(blank=True)
+  usage = models.TextField(blank=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
   available = models.BooleanField(default=True)
   created = models.DateTimeField(auto_now_add=True)
