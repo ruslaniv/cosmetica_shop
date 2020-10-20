fa287feea52ff9fcc9bb5c61ba1b5c79152fee2d
rulsaniv <ruslan@rsln.dev>
2020-10-20 22:53:31 +0700

    Style cart detail page
    Restyle product detail and product list pages. Refactor CartAddProduct
    form.

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]  # диапазо
 
 
 class CartAddProductForm(forms.Form):
-  quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+  quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='Количество')
   override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
