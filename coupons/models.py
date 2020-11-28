c712fed7ebcb945b2881ffb6cbaa31d5afcaf5ad
rulsaniv <ruslan@rsln.dev>
2020-11-28 23:11:40 +0700

    Refactor coupon app model.py
    Add verbose_name attribute

--- coupons/models.py
+++ coupons/models.py
@@ -3,11 +3,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=50,
-                            unique=True)
+    code = models.CharField(max_length=50, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
-    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name='скидка')
     active = models.BooleanField()
 
     def __str__(self):
