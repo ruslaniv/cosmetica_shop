43e89cf116170d5360c75c01692baa30d5799a17
rulsaniv <ruslan@rsln.dev>
2020-12-08 14:15:19 +0700

    Add coupon into the current session
    Add methods to handle coupon in the cart.

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -11,6 +12,7 @@ class Cart(object):
     if not cart:
       cart = self.session[settings.CART_SESSION_ID] = {}  # если нет - инициализация пустой корзины
     self.cart = cart
+    self.coupon_id = self.session.get('coupon_id') # инициализация купона в текущей сессии
 
   def add(self, product, quantity=1, override_quantity=False):  # добавление товара
     product_id = str(product.id)  # ключ для словаря, который будет сохранять товарную позицию
@@ -54,3 +56,20 @@ class Cart(object):
   def clear(self):  # сброс сессии
     del self.session[settings.CART_SESSION_ID]
     self.save()
+
+  @property
+  def coupon(self):     # метод определяется как свойство (property). Если в сессии присутствует атрибут
+    if self.coupon_id:  # coupon_id, то создается новый экземпляр класса Купон с данным ID
+      try:
+        return Coupon.objects.get(id=self.coupon_id)
+      except Coupon.DoesNotExist:
+        pass
+    return None
+
+  def get_discount(self): # размер скидки
+    if self.coupon:
+      return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+    return Decimal(0)
+
+  def get_total_price_after_discount(self): # всего с учетом скидки
+    return self.get_total_price() - self.get_discount()
