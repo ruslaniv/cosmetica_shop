a50b390c8e8bc128c501bf63491f71c7db87721b
rulsaniv <ruslan@rsln.dev>
2021-01-09 20:15:41 +0700

    Add coupon to cart page

--- coupons/models.py
+++ coupons/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=50, unique=True, verbose_name="Купон")
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name='скидка')
